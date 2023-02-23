a19dca0450bc25e4e94b85f16a78446ebbab62f3
Aabed <ahmad.aabed.m@gmail.com>
2023-02-24 01:51:02 +0200

    Update server.js

--- express/server.js
+++ express/server.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.get('/400', (req, res) => res.status(400);
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
